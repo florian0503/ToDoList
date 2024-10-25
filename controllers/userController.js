de8c89ed10763af8bfccb87f3311890e2d14ac1f
BillyRogier <billy.rogier@free.fr>
2024-10-25 14:40:07 +0200

    crud users

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,13 +2,36 @@ const User = require('../models/User')
 
 exports.getUsers = async (req, res) => {
   const users = await User.find()
+
   res.json(users)
 }
 
+exports.getUser = async (req, res) => {
+  const { id } = req.params
+
+  const user = await User.findById(id)
+
+  res.json(user)
+}
+
 exports.createUser = async (req, res) => {
-  const user = new User(req.body)
+  const user = await User.create(req.body)
 
-  await user.save()
+  res.json(user)
+}
 
-  res.status(201).json(user)
+exports.updateUser = async (req, res) => {
+  const { id } = req.params
+
+  const user = await User.findByIdAndUpdate(id, req.body)
+
+  res.json(user)
+}
+
+exports.deleteUser = async (req, res) => {
+  const { id } = req.params
+
+  const user = await User.findByIdAndDelete(id)
+
+  res.json(user)
 }
--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  email: { type: String },
+  password: { type: String },
 })
 
 module.exports = mongoose.model('User', UserSchema)
